370ce33500e2ea9dfc8e169a3180c439b01231d2
Janara3 <tashievajanara123@gmail.com>
2022-09-04 23:12:14 +0600

    Initial commit

---
+++ src/daysofweek/Main.java
@@ -0,0 +1,12 @@
+package daysofweek;
+
+public class Main {
+    public static void main(String[] args){
+    DaysOfWeek days1 = DaysOfWeek.MONDAY;
+    DaysOfWeek days2 = DaysOfWeek.TUESDAY;
+    DaysOfWeek days3 = DaysOfWeek.WEDNESDAY;
+        System.out.println(days1.getDay());
+        System.out.println(days2.getDay());
+        System.out.println(days3.getDay());
+    }
+}
